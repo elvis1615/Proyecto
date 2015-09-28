f218dc126c7e8176062055352911a4512eec9d54
elvis1615 <elvis_rolando@hotmail.es>
2015-09-28 11:18:41 -0600

    pruebas
    pruebas

--- Prueba/Assets/Scripts/contron.cs
+++ Prueba/Assets/Scripts/contron.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class contron : MonoBehaviour {
     Rigidbody2D R;
     public float velocidad;
+    public Transform aux;
    
 
     // Use this for initialization
     void Start () {
         R = GetComponent<Rigidbody2D>();
-	}
+        R.velocity = new Vector2(velocidad, R.velocity.y);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,16 +24,27 @@ public class contron : MonoBehaviour {
 
         float a = Input.GetAxis("Horizontal");
 
-        R.velocity = new Vector2(a * velocidad, R.velocity.y);
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, -transform.up);
-        Quaternion rot = Quaternion.identity;
-        if (ray) {
-            Debug.DrawRay(transform.position, -transform.up, Color.blue, 0.1f);
-            rot = Quaternion.FromToRotation(transform.up,ray.normal) * transform.rotation;
+        if (Input.GetMouseButton(0)) {
+            R.velocity = new Vector3(velocidad, R.velocity.y,0);
+            R.AddForce(new Vector2(0,500));            
         }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 0.1f);
+        R.velocity = new Vector2(a * velocidad, R.velocity.y);
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, -transform.up,10);
+        Quaternion rot = Quaternion.identity;
+        if (ray)
+        {
+            Debug.DrawRay(transform.position, -transform.up, Color.blue, 0.1f);
+            rot = Quaternion.FromToRotation(transform.up, ray.normal) * transform.rotation;
 
+        }
+
+        else if(ray.distance>0.5) {
+            rot = Quaternion.Euler(0,0,0);
+        }
+
+       transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 0.1f);
+        
     }
 
 
